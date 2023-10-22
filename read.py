91038691bb7e9f8fe77d5a500f142728b7055fe6
Sunny990931 <sunny990931@gmail.com>
2023-10-22 12:21:09 +0800

    calc avg

--- read.py
+++ read.py
@@ -5,6 +5,10 @@ with open('reviews.txt', 'r') as f :
 		data.append(line)
 		count += 1
 		if count % 1000 == 0 : # %用來求餘數
-		print(len(data))
-print(len(data))
-print(data[0])
+			print(len(data))
+print('檔案讀取完了， 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data :
+	sum_len = sum_len + len(d)
+print('留言的平均是', sum_len/len(data))
